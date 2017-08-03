dcbfe63bd49ab18256cd3c0693e4d5d105169ce5
adriano <eu@email.com.br>
2017-08-02 22:56:11 -0300

    novas implementações

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,5 +1,6 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 
@@ -23,6 +24,7 @@ public class Sessao {
 	@ManyToOne
 	private Filme filme;
 
+	private BigDecimal preco;
 	/** 
 	 * @deprecated 
 	 */
@@ -35,6 +37,7 @@ public class Sessao {
 		this.horario = horario;
 		this.sala = sala;
 		this.filme = filme;
+		this.preco = sala.getPreco().add(filme.getPreco());
 	}
 
 	public Integer getId() {
@@ -71,6 +74,14 @@ public class Sessao {
 	
 	public LocalTime getHorarioTermino(){
 		return this.horario.plus(filme.getDuracao().toMinutes(), ChronoUnit.MINUTES);
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
 	}
 	
 }
---
+++ src/test/java/ingresso/GerenciadorDeSessaoTest.java
@@ -0,0 +1,81 @@
+package ingresso;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.validacao.GerenciadorDeSessao;
+
+public class GerenciadorDeSessaoTest {
+	
+	@Test
+	public void garanteQueNaoDevePermitirSessaoNoMesmoHorario() {
+		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", BigDecimal.ONE);
+		LocalTime horario = LocalTime.parse("10:00:00");
+		
+		Sala sala = new Sala("Eldorado - IMAX", BigDecimal.ONE);
+		List<Sessao> sessoes = Arrays.asList(new Sessao(horario, sala, filme));
+		
+		Sessao sessao = new Sessao(horario, sala, filme);
+		
+		GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoes);
+		
+		Assert.assertFalse(gerenciador.cabe(sessao));
+	}
+	
+	@Test
+	public void garanteQueNaoDevePermitirSessoesTerminandoDentroDoHorarioDeUmaSessaoJaExistente() {
+		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", BigDecimal.ONE);
+		LocalTime horario = LocalTime.now(); // LocalTime.parse("10:00:00");
+		
+		Sala sala = new Sala("Eldorado - IMAX", BigDecimal.ONE);
+		List<Sessao> sessoes = Arrays.asList(new Sessao(horario, sala, filme));
+		
+		Sessao sessao = new Sessao(horario.minusHours(1), sala, filme);
+		
+		GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoes);
+		
+		Assert.assertFalse(gerenciador.cabe(sessao));
+	}
+	
+	@Test
+	public void garanteQueNaoDevePermitirSessoesIniciandoDentroDoHorarioDeUmaSessaoJaExistente() {
+		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", BigDecimal.ONE);
+		LocalTime horario = LocalTime.parse("10:00:00");
+		Sala sala = new Sala("Eldorado - IMAX", BigDecimal.ONE);
+		
+		List<Sessao> sessoes = Arrays.asList(new Sessao(horario, sala, filme));
+		
+		GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoes);
+		
+		Assert.assertFalse(gerenciador.cabe(new Sessao(horario.plusHours(1), sala, filme)));
+	}
+	
+	@Test
+	public void garanteQueDevePermitirUmaInsercaoEntreDoisFilmes() {
+		
+		Sala sala = new Sala("Eldorado - IMAX", BigDecimal.ONE);
+		
+		Filme filme1 = new Filme("Rogue One", Duration.ofMinutes(90), "SCI-FI", BigDecimal.ONE);
+		LocalTime dezHoras = LocalTime.parse("10:00:00");
+		Sessao sessaoDasDez = new Sessao(dezHoras, sala, filme1);
+		
+		Filme filme2 = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", BigDecimal.ONE);
+		LocalTime dezoitoHoras = LocalTime.parse("18:00:00");
+		Sessao sessaoDasDezoito = new Sessao(dezoitoHoras, sala, filme2);
+		
+		List<Sessao> sessoes = Arrays.asList(sessaoDasDez, sessaoDasDezoito);
+		
+		GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoes);
+		
+		Assert.assertTrue(gerenciador.cabe(new Sessao(LocalTime.parse("13:00:00"), sala, filme2)));
+	}
+}
